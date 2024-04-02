63c52dfa91b8ea2c29f1da8e369553165fb3ba51
gulsah <gmutluayyigit@gmail.com>
2024-04-02 19:25:12 +0300

    sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import "./sass/app.scss";
+import data from "./data";
+import Card from "./components/Card";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <Header />
+    {
+      data.map(({id, name, job, img, comment}) => (
+        <Card key={id} name={name} job={job} img={img} comment={comment} />
+      ))
+
+    }
+ 
+    </>
   );
 }
 
